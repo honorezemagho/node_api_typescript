b50a3323a7a635f1da65e8e1f3e69a73376b7678
honorezemagho <honorezemagho@gmail.com>
2021-06-03 18:35:58 +0100

    add prefix /api to all routes

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { TransformInterceptor } from './transform.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new TransformInterceptor());
+  app.setGlobalPrefix('api');
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT);
   console.log(`The Server is listening on ${PORT}`);
